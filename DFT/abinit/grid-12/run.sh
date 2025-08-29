6bbc26b4030b752dd8b1d622d36c2f57003e1db0
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-08-29 23:02:16 +0200

    fix(dft): rm files & grep

--- DFT/abinit/grid-12/run.sh
+++ DFT/abinit/grid-12/run.sh
@@ -6,10 +6,10 @@ for q in {2..72}; do
     # generate abinit script
     cat ddb-gkk.abi | sed s/%QIDX%/$q/ > $ABI_SCRIPT
     # run the abinit script
-    time { mpirun -n 4 abinit $ABI_SCRIPT | tee logs/Q$q.log | grep -E "== DATASET|frozen|Perturbation|wfk_read|dfpt_scf"; }
+    time { mpirun -n 4 abinit $ABI_SCRIPT | tee logs/Q$q.log | grep -E "== DATASET|frozen part|Perturbation|About to read|dfpt_scf|writing gkk"; }
     # remove heavy wavefunctions
-    rm out/Q${i}_DS1_1WF*
-    rm out/Q${i}_DS1_DEN*
+    rm out/Q${q}_DS1_1WF*
+    rm out/Q${q}_DS1_DEN*
     # remove the script
     rm $ABI_SCRIPT
 done
