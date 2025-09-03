9d812171ca7702d957aa5a1f0285edb165740483
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-09-03 15:18:27 +0200

    refactor(dft): vf & meff computation

--- DFT/fsurf.jl
+++ DFT/fsurf.jl
@@ -1,55 +1,55 @@
-using Scanf
 using CairoMakie
 update_theme!(theme_latexfonts());
 update_theme!(Theme(fontsize=22));
 
+using Scanf
 using GeometryBasics
 using LinearAlgebra
 using Interpolations
 using Meshing
 
 nbands = 18
-nkpts = 7236
-bands = Matrix{Float64}(undef, nkpts, nbands)
-
 eV_in_Ha = 27.211396641308;
 joules_in_Hartree = 4.359748199E-18; # J/Ha
 ħ = 1.054571817e-34; # J*s
+planck = 6.62607015e-34;
 m_e = 9.1093837139e-31; # Kg
 
-function skiplines(io::IO, n::Int) 
+function skiplines(io::IO, n::Int)
     for _ ∈ 1:n
-        skipchars(c -> c != '\n', io);
-        read(io, Char); # actually skip the newline
+        skipchars(c -> c != '\n', io)
+        read(io, Char) # actually skip the newline
     end
 end
 
 begin
-    f = open("./DFT/data/fsurf.bxsf", "r")
-    skiplines(f, 9);
-    _, Ef = @scanf(f, " Fermi Energy: %f", Float64);
+    f = open("./DFT/data/fsurf72.bxsf", "r")
+    skiplines(f, 9)
+    _, Ef = @scanf(f, " Fermi Energy: %f", Float64)
     skiplines(f, 6)
-    _, nbands, nx, ny, nz = @scanf(f, " %d %d %d %d", Int32, Int32, Int32, Int32);
-    nkpts = nx * ny * nz;
-    bands = Vector{Array{Float64}}(undef, nbands);
-    skiplines(f, 2);
-    basis = Matrix{Float64}(undef, 3, 3);
+    _, nbands, nx, ny, nz = @scanf(f, " %d %d %d %d", Int32, Int32, Int32, Int32)
+    nkpts = nx * ny * nz
+    bands = Vector{Array{Float64}}(undef, nbands)
+    skiplines(f, 2)
+    basis = Matrix{Float64}(undef, 3, 3)
     for i ∈ 1:3
-        _, basis[1,i], basis[2,i], basis[3,i] = @scanf(f," %f %f %f", Float64, Float64, Float64);
+        _, basis[1, i], basis[2, i], basis[3, i] = @scanf(f, " %f %f %f", Float64, Float64, Float64)
     end
-    sz = norm(basis[:,1]);
+    inv_basis = inv(basis)
+    a = inv_basis[1, 2] * 2 # Ang
+    sz = norm(basis[:, 1])
     for b ∈ 1:nbands
-        skiplines(f,2);
-        bands[b] = data = Array{Float64}(undef, nx, ny, nz);
+        skiplines(f, 2)
+        bands[b] = data = Array{Float64}(undef, nx, ny, nz)
         for i ∈ 1:nx
             for j ∈ 1:ny
                 for k ∈ 1:nz
-                    _, data[i,j,k] = @scanf(f, " %f", Float64)
+                    _, data[i, j, k] = @scanf(f, " %f", Float64)
                 end
             end
         end
     end
-    close(f); 
+    close(f)
 end
 
 begin
@@ -57,86 +57,121 @@ begin
     # => must exclude last k-points in each direction
     cnt = 0
     for b ∈ 11:14
-        data = bands[b];
-        @views cnt += @views count(E -> E > Ef, data[1:end-1, 1:end-1, 1:end-1]) 
+        data = bands[b]
+        @views cnt += @views count(E -> E > Ef, data[1:end-1, 1:end-1, 1:end-1])
     end
-    periodic_nkpts = (nx-1)*(ny-1)*(nz-1)
+    periodic_nkpts = (nx - 1) * (ny - 1) * (nz - 1)
     println("n_h = $(cnt/periodic_nkpts)")
 end
 
-E = bands[11];
 
 begin
-    dx = 1/(nx-1.0);
-    dy = 1/(ny-1.0);
-    dz = 1/(nz-1.0);
-    X = LinRange(0,1,nx);
-    Y = LinRange(0,1,ny);
-    Z = LinRange(0,1,nz);
-    @views itp = interpolate(E, BSpline(Cubic(Periodic(OnGrid()))));
-    itp = scale(itp, X, Y, Z);
+    E = bands[11]
+    X = LinRange(0, 1, nx)
+    Y = LinRange(0, 1, ny)
+    Z = LinRange(0, 1, nz)
+    @views itp = interpolate(E, BSpline(Cubic(Periodic(OnGrid()))))
+    itp = scale(itp, X, Y, Z)
     # itp = extrapolate(itp, Periodic(OnCell()));
 end;
 
 begin
-    inv_basis = inv(basis);
+    X = LinRange(0, 1, nx)
+    Y = LinRange(0, 1, ny)
+    Z = LinRange(0, 1, nz)
 
-    X = LinRange(0,1,nx)
-    Y = LinRange(0,1,ny)
-    Z = LinRange(0,1,nz)
+    min_v = fill(+Inf64, 2)
+    max_v = fill(-Inf64, 2)
 
-    weighted_iM = [zeros(3,3), zeros(3,3), zeros(3,3), zeros(3,3)];
-    weighted_vf = zeros(4);
-    total_areas = zeros(4);
+    weighted_v1 = zeros(2)
+    weighted_v2 = zeros(2)
 
-    GLp = [4 1 1; 1 4 1; 1 1 4]/6;
-    GLw = [1, 1, 1]/3;
-    nGLp = size(GLp)[2];
+    weighted_im_cond = zeros(2)
 
-    for i ∈ 1:4
-        E = bands[i+10];
-        vertices, faces = isosurface(E, MarchingCubes(iso=Ef), X, Y, Z);
-        vertices_as_mat = reshape(reinterpret(Float64, vertices), (3,:));
-        @views itp = interpolate(E, BSpline(Cubic(Periodic(OnCell()))));
-        itp = scale(itp, X, Y, Z);
+    total_areas = zeros(2)
+
+    GLp = [4 1 1; 1 4 1; 1 1 4] / 6
+    GLw = [1, 1, 1] / 3
+    nGLp = size(GLp)[2]
+
+    for i ∈ 1:2
+        E = bands[2*i+10]
+        vertices, faces = isosurface(E, MarchingCubes(iso=Ef), X, Y, Z)
+        vertices_as_mat = reshape(reinterpret(Float64, vertices), (3, :))
+        @views itp = interpolate(E, BSpline(Cubic(Periodic(OnGrid()))))
+        itp = scale(itp, X, Y, Z)
+
         for (a, b, c) ∈ faces
-            v1 = basis*collect(vertices[b] .- vertices[a]);
-            v2 = basis*collect(vertices[c] .- vertices[a]);
+            v1 = basis * collect(vertices[b] .- vertices[a])
+            v2 = basis * collect(vertices[c] .- vertices[a])
             triangle_area = 0.5 * norm(cross(v1, v2))
 
-            @views integration_points = vertices_as_mat[:, [a,b,c]] * GLp;
-            vel = mapslices(pos -> norm(inv_basis * Interpolations.gradient(itp, pos...)), integration_points, dims=1) * GLw;
+            @views integration_points = vertices_as_mat[:, [a, b, c]] * GLp
 
-            # baricenter = (vertices[a] .+ vertices[b] .+ vertices[c])./3;
-            # vel = inv_basis * Interpolations.gradient(itp, baricenter...);
-            # iM = inv_basis * inv_basis * Interpolations.hessian(itp, baricenter...);
-            # weighted_iM[i] += triangle_area * iM;
-            weighted_vf[i] += triangle_area * norm(vel);
-            total_areas[i] += triangle_area;
+            vel1 = mapslices(integration_points, dims=1) do pos
+                norm(inv_basis * Interpolations.gradient(itp, pos...))
+            end
+
+            vel2 = mapslices(integration_points, dims=1) do pos
+                v = inv_basis * Interpolations.gradient(itp, pos...)
+                return v ⋅ v
+            end
+
+            iM = map(eachcol(integration_points)) do pos
+                inv_basis * inv_basis * Interpolations.hessian(itp, pos...)
+            end
+
+            im_cond = map(tr, iM)
+            # m_therm = map(cbrt ∘ inv ∘ det, iM)
+
+            min_v[i] = min(min_v[i], vel1...)
+            max_v[i] = max(max_v[i], vel1...)
+
+            total_areas[i] += triangle_area
+            weighted_v1[i] += triangle_area * (GLw ⋅ vel1)
+            weighted_v2[i] += triangle_area * (GLw ⋅ vel2)
+
+            weighted_im_cond[i] += triangle_area * (GLw ⋅ im_cond)
         end
     end
 
-    weighted_iM .*= (joules_in_Hartree * m_e) / (ħ^2 * 1e20); # 1/mₑ
-    weighted_vf .*= 1e-10 * joules_in_Hartree / ħ; # m/s
+    # We don't divide by hbar because our basis matrix
+    # is already the reciprocal basis divided by 2π !!
 
-    iM = sum(weighted_iM, dims=1)[1] / sum(total_areas);
-    vf = sum(weighted_vf) / sum(total_areas);
+    v_factor = 1e-10 * joules_in_Hartree / planck # Ha*Ang => m/s
+    weighted_v1 .*= v_factor
+    weighted_v2 .*= v_factor^2
+    min_v .*= v_factor
+    max_v .*= v_factor
+
+    # division by 3 for the average of the traces
+    m_factor = planck^2 * 1e20 / (joules_in_Hartree * m_e)     # 1/(Ha*Ang^2) => mₑ
+    im_factor = (joules_in_Hartree * 1e-20 * m_e) / planck^2   # Ha*Ang^2 => 1/mₑ
+    weighted_im_cond ./= m_factor * 3
+
+    S = sum(total_areas)
+    vel1 = sum(weighted_v1) / S
+    vel2 = sum(weighted_v2) / S
+    imeff = weighted_im_cond ./ total_areas
+    m = 2 / sum(imeff)
+
+    print("<|vf|> = $(vel1*1e-6) km/ms\n⎷(<|vf|²>) = $(sqrt(vel2)*1e-6) km/ms\n")
+    print("m_c = 3/<Tr(inv(M))> = $(1 ./ imeff) => $m  (mₑ)\n")
 end
 
-
 begin
-    X = LinRange(0,1,nx)
-    Y = LinRange(0,1,ny)
-    Z = LinRange(0,1,nz)
-    @views vertices, faces = isosurface(E, MarchingCubes(iso=Ef), X, Y, Z);
-    vertices_as_mat = reshape(reinterpret(Float64, vertices), (3,:))'
-    faces_as_mat = reshape(reinterpret(Int, faces), (3,:))'
+    X = LinRange(0, 1, nx)
+    Y = LinRange(0, 1, ny)
+    Z = LinRange(0, 1, nz)
+    @views vertices, faces = isosurface(E, MarchingCubes(iso=Ef), X, Y, Z)
+    vertices_as_mat = reshape(reinterpret(Float64, vertices), (3, :))'
+    faces_as_mat = reshape(reinterpret(Int, faces), (3, :))'
 
-    upper = sum(basis, dims=2);
-    fig = Figure(size=(600,600))
+    upper = sum(basis, dims=2)
+    fig = Figure(size=(600, 600))
     ax = Axis3(
-        fig[1,1], 
-        aspect=(1,1,1),
+        fig[1, 1],
+        aspect=(1, 1, 1),
         azimuth=1.2π,
         elevation=0.12π,
     )
@@ -151,9 +186,9 @@ begin
         1 0 1;
         1 1 1;
         0 1 1;
-        0 0 1;
-    ] * basis;
-    lines!(ax, path1, color=:black);
+        0 0 1
+    ] * basis
+    lines!(ax, path1, color=:black)
 
     path2 = [
         1 0 0;
@@ -161,8 +196,8 @@ begin
         1 1 0;
         1 1 1;
         0 1 0;
-        0 1 1;
-    ]*basis;
+        0 1 1
+    ] * basis
     linesegments!(ax, path2, color=:black)
     mesh!(ax, vertices_as_mat, faces_as_mat)
 
